51196b4b66826f135f489908626190af7e7e9878
Dmitriy <plotnikovdi95@gmail.com>
2023-01-24 10:17:26 +0300

    Игра улучшена

--- src/components/ClassComponent/ClassComponent.jsx
+++ src/components/ClassComponent/ClassComponent.jsx
@@ -6,7 +6,7 @@ export class ClassComponent extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      result: 'Результат',
+      result: `Угадайте число в диапазоне от ${this.props.min} до ${this.props.max}`,
       userNumber: '',
       count: 0,
       randomNumber:
@@ -16,40 +16,52 @@ export class ClassComponent extends React.Component {
     };
   }
 
-  handleClick = (e) => {
-    this.setState(() => ({
-      count: 0,
-      guessed: false,
-      randomNumber:
-        Math.floor(Math.random() * (this.props.max - this.props.min)) +
-        this.props.min,
-      result: 'Результат',
-    }));
-  };
   handleSubmit = (e) => {
     e.preventDefault();
 
-    this.setState((state) => ({
-      count: state.count + 1,
-    }));
     this.setState((state) => {
-      if (!state.userNumber) return state;
+      if (state.guessed) {
+        return {
+          count: 0,
+          guessed: false,
+          randomNumber:
+            Math.floor(Math.random() * (this.props.max - this.props.min)) +
+            this.props.min,
+          result: `Угадайте число в диапазоне от ${this.props.min} до ${this.props.max}`,
+        };
+      }
+
+      if (state.userNumber === '') {
+        return {
+          result: 'Введите число',
+        };
+      }
+      if (
+        state.userNumber > this.props.max ||
+        state.userNumber < this.props.min
+      ) {
+        return {
+          result: `Ввидите число из диапозона от ${this.props.min} до ${this.props.max}`,
+        };
+      }
 
       if (state.userNumber > state.randomNumber) {
         return {
           result: `${state.userNumber} больше загаданного`,
           userNumber: '',
+          count: state.count + 1,
         };
       }
       if (state.userNumber < state.randomNumber) {
         return {
           result: `${state.userNumber} меньше загаданного`,
           userNumber: '',
+          count: state.count + 1,
         };
       }
       return {
         result: `Вы угадали, загаданное число ${state.userNumber},
-         попыток ${state.count}`,
+         попыток ${state.count + 1} `,
         userNumber: '',
         guessed: true,
       };
@@ -64,7 +76,6 @@ export class ClassComponent extends React.Component {
     return (
       <div className={style.game}>
         <p className={style.result}>{this.state.result}</p>
-
         <form className={style.form} onSubmit={this.handleSubmit}>
           <label className={style.label} htmlFor='user_number'>
             Угадай число
@@ -75,13 +86,11 @@ export class ClassComponent extends React.Component {
             id='user_number'
             onChange={this.handleChange}
             value={this.state.userNumber}
+            disabled={this.state.guessed}
           />
-          <button className={style.btn}>Угадать</button>
-          {this.state.guessed && (
-            <button onClick={this.handleClick} className={style.btn}>
-              Сыграть ещё
-            </button>
-          )}
+          <button className={style.btn}>
+            {this.state.guessed ? 'Сыграть ещё' : 'Угадать'}
+          </button>
         </form>
       </div>
     );
